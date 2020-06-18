23efde6aff9b98d73a870d1113fb7f95f928b5ec
Jacob <perezjacob2@gmail.com>
2020-06-18 18:56:31 -0500

    todo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
 import React, {Component} from 'react';
-
-
+import Header from './components/layout/Header';
+import Todos from './components/Todos';
+import './App.css';
 
 class App extends Component {
+  state = {
+    todos:[
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Run',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Eat Dinner',
+        completed: true
+      }
+    ]
+  }
+  
+markComplete = (id) => {
+        this.setState({todos: this.state.todos.map(todo=> {
+          if(todo.id === id) {
+            todo.completed =!todo.completed
+          }
+          return todo;
+        })})  
+}
+
+delTodo = (id) => {
+ this.setState ({ todos:  [...this.state.todos.filter(todo => todo.id
+  !==id)]})
+}
+
   render() {
     return(
-      
     <div className="App">
-      
-      <img src="https://image-cdn.essentiallysports.com/wp-content/uploads/20200507172357/mj-featureee.jpeg">
-      </img>
+      <Header/>
+      <Todos todos={this.state.todos} markComplete={this.markComplete} 
+      delTodo={this.delTodo}/>
     </div>
   );
 }
