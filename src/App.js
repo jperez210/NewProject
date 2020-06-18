5f4115535f569dde29e90d93e66b6ebbe099991c
Jacob <perezjacob2@gmail.com>
2020-06-18 13:24:25 -0500

    update

--- src/App.js
+++ src/App.js
@@ -2,16 +2,11 @@ import React, {Component} from 'react';
 
 
 
+class App extends Component {
+  render() {
     return(
+      
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
       
       <img src="https://image-cdn.essentiallysports.com/wp-content/uploads/20200507172357/mj-featureee.jpeg">
       </img>
